bd9d4219692131974e1d56cbc78c041c173930ff
enrloport <e.l.o.universidad@gmail.com>
2024-02-11 09:11:06 +0100

    split_dataset cloudcast created

---
+++ files/cloudcast_functions/split_dataset.jl
@@ -0,0 +1,14 @@
+
+function split_data_cloudcast(;data, train_length, test_length, target_pixel, radius, step=1)
+
+    d,tp,rd,trl,tel = data,target_pixel, radius, train_length, test_length
+
+    train_x = d[1:trl                    , tp[1]-rd:tp[1]+rd    , tp[2]-rd:tp[2]+rd ]
+    test_x  = d[trl+1:trl+tel+1          , tp[1]-rd:tp[1]+rd    , tp[2]-rd:tp[2]+rd ]
+    train_y = d[1+step:trl+step          , tp[1]                , tp[2]]
+    test_y  = d[trl+1+step:trl+tel+1+step, tp[1]                , tp[2]]
+
+    
+    return cc_to_int(train_x), cc_to_int(train_y), cc_to_int(test_x), cc_to_int(test_y)
+
+end
