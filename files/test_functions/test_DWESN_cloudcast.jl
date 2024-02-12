ef0ebbdd9171f468dc032d382b2f248d94cc4391
enrloport <e.l.o.universidad@gmail.com>
2024-02-12 12:11:17 +0100

    deepE replaced by dwE

--- files/test_functions/test_DWESN_cloudcast.jl
+++ files/test_functions/test_DWESN_cloudcast.jl
@@ -1,23 +1,23 @@
-# Function to test an already trained deepESN struct
-function __do_test_DWESN_cloudcast!(deepE, args::Dict)
+# Function to test an already trained deepwideESN struct
+function __do_test_DWESN_cloudcast!(dwE, args::Dict)
     test_length = args[:test_length]
 
     classes_Y    = Array{Tuple{Float64,Int,Int}}[]
     wrong_class  = []
-    deepE.Y      = []
+    dwE.Y      = []
 
     f = args[:gpu] ? (u) -> CuArray(reshape(u, :, 1)) : (u) -> reshape(u, :, 1)
 
     for t in 1:test_length
 
-        _step_cloudcast(deepE, args[:test_data], t, f)
+        _step_cloudcast(dwE, args[:test_data], t, f)
 
-        x = vcat(f(args[:test_data][t,:,:]), [ _e.x for l in deepE.layers for _e in l.esns]...  , f([1]) )
+        x = vcat(f(args[:test_data][t,:,:]), [ _e.x for l in dwE.layers for _e in l.esns]...  , f([1]) )
 
 
         pairs  = []
         for c in args[:classes]
-            yc = Array(deepE.classes_Routs[c] * x)[1]
+            yc = Array(dwE.classes_Routs[c] * x)[1]
             push!(pairs, (yc, c, args[:test_labels][t]))
         end
         pairs_sorted  = reverse(sort(pairs))
@@ -26,14 +26,14 @@ function __do_test_DWESN_cloudcast!(deepE, args::Dict)
             push!(wrong_class, (args[:test_data][t], pairs_sorted[1], pairs_sorted[2], t ) ) 
         end
 
-        push!(deepE.Y,[Int8(pairs_sorted[1][2]) ;])
+        push!(dwE.Y,[Int8(pairs_sorted[1][2]) ;])
         push!(classes_Y, pairs )
 
     end
 
-    deepE.wrong_class= wrong_class
-    deepE.classes_Y  = classes_Y
-    deepE.Y_target   = args[:test_labels]
-    deepE.error      = length(wrong_class) / length(classes_Y)
+    dwE.wrong_class= wrong_class
+    dwE.classes_Y  = classes_Y
+    dwE.Y_target   = args[:test_labels]
+    dwE.error      = length(wrong_class) / length(classes_Y)
 
 end
--- files/train_functions/train_DWESN_cloudcast.jl
+++ files/train_functions/train_DWESN_cloudcast.jl
@@ -1,36 +1,36 @@
-function _step_cloudcast(deepE, data,t,f)
-    for _esn in deepE.layers[1].esns
+function _step_cloudcast(dwE, data,t,f)
+    for _esn in dwE.layers[1].esns
         a = data[t,:,:]
         __update(_esn, a, f )
     end
 
-    for i in 2:length(deepE.layers)
-        for _esn in deepE.layers[i].esns
-            v = vcat([_e.x for _e in deepE.layers[i-1].esns ]...)
+    for i in 2:length(dwE.layers)
+        for _esn in dwE.layers[i].esns
+            v = vcat([_e.x for _e in dwE.layers[i-1].esns ]...)
             __update(_esn, v , f )
         end
     end
 end
 
-function __fill_X_DWESN_cloudcast!(deepE, args::Dict )
+function __fill_X_DWESN_cloudcast!(dwE, args::Dict )
 
     f = args[:gpu] ? (u) -> CuArray(reshape(u, :, 1)) : (u) -> reshape(u, :, 1)
 
     for t in 1:args[:initial_transient]
-        _step_cloudcast(deepE, args[:train_data], t, f)
+        _step_cloudcast(dwE, args[:train_data], t, f)
     end
 
     for t in args[:initial_transient]+1:args[:train_length]
         t_in = t - args[:initial_transient]
-        _step_cloudcast(deepE, args[:train_data], t, f)
+        _step_cloudcast(dwE, args[:train_data], t, f)
 
-        deepE.X[:,t_in] = vcat(f(args[:train_data][t,:,:]), [ _e.x for l in deepE.layers for _e in l.esns]...  , f([1]) )
+        dwE.X[:,t_in] = vcat(f(args[:train_data][t,:,:]), [ _e.x for l in dwE.layers for _e in l.esns]...  , f([1]) )
     end
 end
 
 
-function __make_Rout_DWESN_cloudcast!(deepE,args)
-    X             = deepE.X
+function __make_Rout_DWESN_cloudcast!(dwE,args)
+    X             = dwE.X
     classes       = args[:classes]
     classes_Yt    = Dict( c => zeros(args[:train_length]-args[:initial_transient]) for c in classes )  # New dataset for each class
 
@@ -46,30 +46,30 @@ function __make_Rout_DWESN_cloudcast!(deepE,args)
     end
 
     cudamatrix = args[:gpu] ? CuArray : Matrix
-    deepE.classes_Routs = Dict( c => cudamatrix(transpose((X*transpose(X) + deepE.beta*I) \ (X*classes_Yt[c]))) for c in classes )
+    dwE.classes_Routs = Dict( c => cudamatrix(transpose((X*transpose(X) + dwE.beta*I) \ (X*classes_Yt[c]))) for c in classes )
 end
 
 
-function __do_train_DWESN_cloudcast!(deepE, args)
+function __do_train_DWESN_cloudcast!(dwE, args)
     num   = args[:train_length]-args[:initial_transient]
-    flt = vcat(deepE.layers...)
-    deepE.X = zeros( sum([layer.nodes for layer in flt ]) + (args[:radius]*2+1)^2 + 1, num)
+    flt = vcat(dwE.layers...)
+    dwE.X = zeros( sum([layer.nodes for layer in flt ]) + (args[:radius]*2+1)^2 + 1, num)
 
-    for layer in deepE.layers
+    for layer in dwE.layers
         for _esn in layer.esns
             _esn.x = zeros( _esn.R_size, 1)
         end
     end
 
     if args[:gpu]
-        deepE.X = CuArray(deepE.X)
-        for layer in deepE.layers
+        dwE.X = CuArray(dwE.X)
+        for layer in dwE.layers
             for _esn in layer.esns
                 _esn.x = CuArray( _esn.x)
             end
         end
     end
 
-    __fill_X_DWESN_cloudcast!(deepE,args)
-    __make_Rout_DWESN_cloudcast!(deepE,args)
+    __fill_X_DWESN_cloudcast!(dwE,args)
+    __make_Rout_DWESN_cloudcast!(dwE,args)
 end
