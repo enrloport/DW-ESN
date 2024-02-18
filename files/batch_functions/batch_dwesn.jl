91436a0a3a07be3cf079d3bc51c2de24704433b8
enrloport <e.l.o.universidad@gmail.com>
2024-02-18 08:32:09 +0100

    updates in batch

--- files/batch_functions/batch_dwesn.jl
+++ files/batch_functions/batch_dwesn.jl
@@ -58,11 +58,11 @@ function do_batch_dwesn(_params_esn, _params)
         , "rho" => pe[:rho][1][1]
     )
     cls_nms = string.(p[:classes])
-    if p[:wb] 
+    if p[:wb]
+        to_log["conf_mat"] = Wandb.wandb.plot.confusion_matrix(
+            y_true = p[:test_labels][1:p[:test_length]], preds = [x[1] for x in dwE.Y], class_names = cls_nms
+        )
         Wandb.log(p[:lg], to_log )
-        Wandb.log(p[:lg], Dict( "conf_mat"  => Wandb.wandb.plot.confusion_matrix(
-                    y_true = p[:test_labels][1:p[:test_length]], preds = [x[1] for x in dwE.Y], class_names = cls_nms
-                )))
     else
         display(to_log)
         display(confusion_matrix(cls_nms,p[:test_labels], [x[1] for x in dwE.Y]) )
