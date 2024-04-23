5009a6a5e4f0ecd920040d44d8d3fc97e5679e66
enrloport <e.l.o.universidad@gmail.com>
2024-04-23 12:05:13 +0200

    log function deleted from batch_dwesn

--- files/batch_functions/batch_dwesn.jl
+++ files/batch_functions/batch_dwesn.jl
@@ -25,54 +25,19 @@ function do_batch_dwesn(_params_esn, _params)
         push!(layers,layer)
     end   
 
-    tms = @elapsed begin
-        dwE = DWESN(
-            layers = layers
-            ,beta=p[:beta] 
-            ,train_function = p[:train_f]
-            ,test_function = p[:test_f]
-            )
-        tm_train = @elapsed begin
-            dwE.train_function(dwE,p)
-        end
-        tm_test = @elapsed begin
-            dwE.test_function(dwE,p)
-        end
+    dwE = DWESN(
+        layers = layers
+        ,beta=p[:beta] 
+        ,train_function = p[:train_f]
+        ,test_function = p[:test_f]
+        )
+    tm_train = @elapsed begin
+        dwE.train_function(dwE,p)
     end
- 
-    to_log = Dict(
-        "Total time"        => tms
-        ,"Train time"       => tm_train
-        ,"Test time"        => tm_test
-        ,"Error"            => dwE.error
-        ,"Layers"           => p[:layers]
-        , "Sigmoids"        => pe[:sgmds]
-        , "Alphas"          => pe[:alpha]
-        , "Densities"       => pe[:density]
-        , "R_in_densities"  => pe[:Rin_dens]
-        , "Rhos"            => pe[:rho]
-        , "Sigmas"          => pe[:sigma]
-        , "R_scalings"      => pe[:R_scaling]
-        , "reservoirs"      => sum([x[1] for x in p[:layers]])
-        , "nodes" => sum( [ l[1]*l[2] for l in p[:layers] ] )
-	    , "alpha min" => minimum( vcat( pe[:alpha]...) )
-	    , "alpha max" => maximum( vcat( pe[:alpha]...) )
-	    , "density min" => minimum( vcat( pe[:density]...) )
-	    , "density max" => maximum( vcat( pe[:density]...) )
-        , "rho" => pe[:rho][1][1]
-        , "sigma" => pe[:sigma][1][1]
-    )
-    cls_nms = string.(p[:classes])
-    if p[:wb]
-        if p[:confusion_matrix]
-            to_log["conf_mat"] = Wandb.wandb.plot.confusion_matrix(
-                y_true = p[:test_labels][1:p[:test_length]], preds = [x[1] for x in dwE.Y], class_names = cls_nms
-            )
-        end
-        Wandb.log(p[:lg], to_log )
-    else
-        display(to_log)
-        if p[:confusion_matrix] display(confusion_matrix(cls_nms,p[:test_labels], [x[1] for x in dwE.Y]) ) end
+    tm_test = @elapsed begin
+        dwE.test_function(dwE,p)
     end
+    p[:train_time],p[:test_time] = tm_train, tm_test
+
     return dwE
 end
