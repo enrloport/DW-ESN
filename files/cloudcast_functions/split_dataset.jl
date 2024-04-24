72201a205e623b4a466b07f98eb69cecbd33334e
enrloport <e.l.o.universidad@gmail.com>
2024-04-24 11:02:38 +0200

    updates in split_dataset

--- files/cloudcast_functions/split_dataset.jl
+++ files/cloudcast_functions/split_dataset.jl
@@ -3,17 +3,17 @@ function split_data_cloudcast(;data, train_length, test_length, target_pixel, ra
 
     d,tp,rd,trl,tel = data,target_pixel, radius, train_length, test_length
 
-    train_x = d[1:trl                   , tp[1]-rd:tp[1]+rd    , tp[2]-rd:tp[2]+rd ]
-    test_x  = d[trl+1:trl+tel           , tp[1]-rd:tp[1]+rd    , tp[2]-rd:tp[2]+rd ]
+    train_x   = cc_to_int(d[1:trl         , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
+    test_x    = cc_to_int(d[trl+1:trl+tel , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
 
     if typeof(step) == Int
-        train_y = d[1+step:trl+step         , tp[1]                , tp[2]]
-        test_y  = d[trl+1+step:trl+tel+step , tp[1]                , tp[2]]
+        train_y = cc_to_int(d[1+step:trl+step         , tp[1] , tp[2]])
+        test_y  = cc_to_int(d[trl+1+step:trl+tel+step , tp[1] , tp[2]])
     else
-        train_y = [d[1+step[i]:trl+step[i]         , tp[1]                , tp[2]] for i in step]
-        test_y  = [d[trl+1+step[i]:trl+tel+step[i] , tp[1]                , tp[2]] for i in step]
+        train_y = [cc_to_int(d[1+step[i]:trl+step[i]        , tp[1], tp[2]]) for i in step]
+        test_y  = [cc_to_int(d[trl+1+step[i]:trl+tel+step[i], tp[1], tp[2]]) for i in step]
     end
     
-    return cc_to_int(train_x), cc_to_int(train_y[1]), cc_to_int(test_x), cc_to_int(test_y[1])
+    return train_x, train_y, test_x, test_y
 
 end
