de1f16cf93c1f95d85c214203353f972b4ffa0f4
enrloport <e.l.o.universidad@gmail.com>
2024-04-24 13:02:13 +0200

    updates in test_dwesn_cloudcast

--- files/test_functions/test_DWESN_cloudcast_pixel.jl
+++ files/test_functions/test_DWESN_cloudcast_pixel.jl
@@ -1,39 +1,38 @@
 # Function to test an already trained deepwideESN struct
 function __do_test_DWESN_cloudcast_pixel!(dwE, args::Dict)
-    test_length = args[:test_length]
-
-    classes_Y    = Array{Tuple{Float64,Int,Int}}[]
-    wrong_class  = []
-    dwE.Y      = []
-
-    f = args[:gpu] ? (u) -> CuArray(reshape(u, :, 1)) : (u) -> reshape(u, :, 1)
+    test_length   = args[:test_length]
+    classes_Y     = Dict( stp => Array{Tuple{Float64,Int,Int}}[] for stp in args[:step])
+    wrong_class   = Dict( stp => [] for stp in args[:step])
+    dwE.Y         = Dict( stp => [] for stp in args[:step])
+    f             = args[:gpu] ? (u) -> CuArray(reshape(u, :, 1)) : (u) -> reshape(u, :, 1)
 
     for t in 1:test_length
-
         _step_cloudcast(dwE, args[:test_data], t, f)
+        x       = vcat(f(args[:test_data][t,:,:]), [ _e.x for l in dwE.layers for _e in l.esns]...  , f([1]) )
+        pairs   = Dict( stp => [] for stp in args[:step])
 
-        x = vcat(f(args[:test_data][t,:,:]), [ _e.x for l in dwE.layers for _e in l.esns]...  , f([1]) )
+        for stp in args[:step]
+            for c in args[:classes]
+                yc = Array(dwE.classes_Routs[stp][c] * x)[1]
+                push!(pairs[stp], (yc, c, args[:test_labels][stp][t]))
+            end
 
+            pairs_sorted  = reverse(sort(pairs[stp]))
 
-        pairs  = []
-        for c in args[:classes]
-            yc = Array(dwE.classes_Routs[c] * x)[1]
-            push!(pairs, (yc, c, args[:test_labels][t]))
+            if pairs_sorted[1][2] != pairs_sorted[1][3]
+                push!(wrong_class[stp], (args[:test_data][t], pairs_sorted[1], pairs_sorted[2], t ) ) 
+            end
+
+            push!(dwE.Y[stp],[Int8(pairs_sorted[1][2]) ;])
+            push!(classes_Y[stp], pairs[stp] )
         end
-        pairs_sorted  = reverse(sort(pairs))
-        
-        if pairs_sorted[1][2] != pairs_sorted[1][3]
-            push!(wrong_class, (args[:test_data][t], pairs_sorted[1], pairs_sorted[2], t ) ) 
-        end
 
-        push!(dwE.Y,[Int8(pairs_sorted[1][2]) ;])
-        push!(classes_Y, pairs )
 
     end
 
     dwE.wrong_class= wrong_class
     dwE.classes_Y  = classes_Y
     dwE.Y_target   = args[:test_labels]
-    dwE.error      = length(wrong_class) / length(classes_Y)
+    dwE.error      = length(wrong_class[1]) / length(classes_Y[1])
 
 end
