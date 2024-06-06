f1e021634199dd7803fb1dbc2c43375881269ba2
enrloport <e.l.o.universidad@gmail.com>
2024-06-06 12:36:14 +0200

    updates in desn_cloudcast_pixel

--- runs/cloudcast/deepESN/DESN_cloudcast_pixel_GPU.jl
+++ runs/cloudcast/deepESN/DESN_cloudcast_pixel_GPU.jl
@@ -7,7 +7,7 @@ all     = ncread(dir*file, "__xarray_dataarray_variable__")
 
 
 # PARAMS
-repit = 2
+repit = 100
 _params = Dict{Symbol,Any}(
      :gpu               => true
     ,:wb                => true
@@ -40,58 +40,62 @@ if _params[:gpu] CUDA.allowscalar(false) end
 if _params[:wb] using Logging, Wandb end
 
 
-for _ in 1:repit
-    dwE=[]
-    _params[:layers] = [(1,300),(1,300)]
-    sd = rand(1:10000)
-    Random.seed!(sd)
+for _l in [2,3,4,5]
 
-    _params_esn = Dict{Symbol,Any}(
-        :R_scaling => [rand(Uniform(0.5,1.5),num_e[1] ) for num_e in _params[:layers]]
-        ,:alpha    => [rand(Uniform(0.3,0.7),num_e[1] ) for num_e in _params[:layers] ]
-        ,:density  => [rand(Uniform(0.1,0.3),num_e[1] ) for num_e in _params[:layers]]
-        ,:Rin_dens => [rand(Uniform(0.1,0.5),num_e[1] ) for num_e in _params[:layers]]
-        ,:rho      => [rand(Uniform(1.0,4.0),num_e[1] ) for num_e in _params[:layers]]
-        ,:sigma    => [rand(Uniform(0.5,1.5),num_e[1] ) for num_e in _params[:layers]]
-        ,:sgmds    => [ [tanh for _ in 1:_params[:layers][i][1]] for i in 1:length(_params[:layers]) ]
-    )
+    for _ in 1:repit
+        dwE=[]
+        _params[:layers] = [(1,300) for _ in 1:_l]
+        sd = rand(1:10000)
+        Random.seed!(sd)
 
-    par = Dict(
-          "Seed"                => sd
-        , "Total nodes"         => sum( map(x -> x[1]*x[2], _params[:layers] ) )
-        , "Layers"              => _params[:layers]
-        , "Train length"        => _params[:train_length]
-        , "Test length"         => _params[:test_length]
-        , "Target pixel"        => _params[:target_pixel]
-        , "Radius"              => _params[:radius]
-        , "Initial transient"   => _params[:initial_transient]
-        , "Sigmoids"            => _params_esn[:sgmds]
-        , "Alphas"              => _params_esn[:alpha]
-        , "Densities"           => _params_esn[:density]
-        , "R_in_densities"      => _params_esn[:Rin_dens]
-        , "Rhos"                => _params_esn[:rho]
-        , "Sigmas"              => _params_esn[:sigma]
-        , "R_scalings"          => _params_esn[:R_scaling]
+        _params_esn = Dict{Symbol,Any}(
+            :R_scaling => [rand(Uniform(0.5,1.5),num_e[1] ) for num_e in _params[:layers]]
+            ,:alpha    => [rand(Uniform(0.3,0.7),num_e[1] ) for num_e in _params[:layers] ]
+            ,:density  => [rand(Uniform(0.1,0.3),num_e[1] ) for num_e in _params[:layers]]
+            ,:Rin_dens => [rand(Uniform(0.1,0.5),num_e[1] ) for num_e in _params[:layers]]
+            ,:rho      => [rand(Uniform(1.0,4.0),num_e[1] ) for num_e in _params[:layers]]
+            ,:sigma    => [rand(Uniform(0.5,1.5),num_e[1] ) for num_e in _params[:layers]]
+            ,:sgmds    => [ [tanh for _ in 1:_params[:layers][i][1]] for i in 1:length(_params[:layers]) ]
         )
-    if _params[:wb]
-        _params[:lg] = wandb_logger(_params[:wb_logger_name])
-        Wandb.log(_params[:lg], par )
+
+        par = Dict(
+            "Seed"                => sd
+            , "Total nodes"         => sum( map(x -> x[1]*x[2], _params[:layers] ) )
+            , "Layers"              => _params[:layers]
+            , "Train length"        => _params[:train_length]
+            , "Test length"         => _params[:test_length]
+            , "Target pixel"        => _params[:target_pixel]
+            , "Radius"              => _params[:radius]
+            , "Initial transient"   => _params[:initial_transient]
+            , "Sigmoids"            => _params_esn[:sgmds]
+            , "Alphas"              => _params_esn[:alpha]
+            , "Densities"           => _params_esn[:density]
+            , "R_in_densities"      => _params_esn[:Rin_dens]
+            , "Rhos"                => _params_esn[:rho]
+            , "Sigmas"              => _params_esn[:sigma]
+            , "R_scalings"          => _params_esn[:R_scaling]
+            )
+        if _params[:wb]
+            _params[:lg] = wandb_logger(_params[:wb_logger_name])
+            Wandb.log(_params[:lg], par )
+        end
+        display(par)
+
+        tm = @elapsed begin
+            dwE = do_batch_dwesn(_params_esn,_params)
+        end
+        _params[:total_time] = tm
+        full_log(_params,_params_esn,dwE)
+
+        if _params[:wb]
+            close(_params[:lg])
+        end
+
+        printime = _params[:gpu] ? "Time GPU: " * string(tm) :  "Time CPU: " * string(tm) 
+        println("Error: ", dwE.error, "\n", printime  )
+
     end
-    display(par)
-
-    tm = @elapsed begin
-        dwE = do_batch_dwesn(_params_esn,_params)
-    end
-    _params[:total_time] = tm
-    full_log(_params,_params_esn,dwE)
-
-    if _params[:wb]
-        close(_params[:lg])
-    end
-
-    printime = _params[:gpu] ? "Time GPU: " * string(tm) :  "Time CPU: " * string(tm) 
-    println("Error: ", dwE.error, "\n", printime  )
-
 end
+
 
 # EOF
