0c3b8d1c46086696aed12870569bfbe8a9705d6a
enrloport <e.l.o.universidad@gmail.com>
2024-06-07 08:55:41 +0200

    updates in batch_dwesn

--- files/batch_functions/batch_dwesn.jl
+++ files/batch_functions/batch_dwesn.jl
@@ -2,7 +2,6 @@
 function do_batch_dwesn(_params_esn, _params)
 
     p,pe  = _params, _params_esn
-    # im_sz = (p[:radius]*2 + 1)^2
     im_sz = p[:input_size]
 
     layers = []
@@ -16,10 +15,11 @@ function do_batch_dwesn(_params_esn, _params)
     push!(layers,layer1)
 
     for l in 2:length(p[:layers])
+        input_sz = p[:input_to_all] ? layers[l-1].nodes + im_sz : layers[l-1].nodes
         layer = layerESN( esns = [
             ESN(
                  R      = new_R(p[:layers][l][2], density=pe[:density][l][i], rho=pe[:rho][l][i], gpu=p[:gpu])
-                ,R_in   = new_R_in(p[:layers][l][2], layers[l-1].nodes, sigma = pe[:sigma][l][i] ,gpu=p[:gpu], density=pe[:Rin_dens][l][i] )
+                ,R_in   = new_R_in(p[:layers][l][2], input_sz, sigma = pe[:sigma][l][i] ,gpu=p[:gpu], density=pe[:Rin_dens][l][i] )
                 ,R_scaling = pe[:R_scaling][l][i], alpha  = pe[:alpha][l][i], rho = pe[:rho][l][i], sigma = pe[:sigma][l][i], sgmd = pe[:sgmds][l][i]
             ) for i in 1:p[:layers][l][1]
         ])
@@ -30,7 +30,8 @@ function do_batch_dwesn(_params_esn, _params)
         layers = layers
         ,beta=p[:beta] 
         ,train_function = p[:train_f]
-        ,test_function = p[:test_f]
+        ,test_function  = p[:test_f]
+        ,input_to_all   = p[:input_to_all]
         )
     tm_train = @elapsed begin
         dwE.train_function(dwE,p)
