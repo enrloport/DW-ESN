1508a1ce25f9b5ef361ab1f8510e792e002e5280
enrloport <e.l.o.universidad@gmail.com>
2024-06-07 08:54:58 +0200

    updates in dwesn struct

--- files/ESN_struct.jl
+++ files/ESN_struct.jl
@@ -24,8 +24,9 @@ end
 
 Base.@kwdef mutable struct DWESN
     layers          ::Vector{layerESN}
-    train_function  ::Function      = __do_train_DWESN!
-    test_function   ::Function      = __do_test_DWESN!
+    input_to_all    ::Bool          = false
+    train_function  ::Function      = __do_train_DWESN_cloudcast!
+    test_function   ::Function      = __do_test_DWESN_cloudcast_pixel!
     X               ::Mtx           = zeros(1,1)
     R_out           ::Mtx           = zeros(1,1)
     beta            ::Float64       = 1.0e-8
