94e75b7c9c88c5ebf723444a2e67a244865a54ad
enrloport <e.l.o.universidad@gmail.com>
2024-06-17 12:14:57 +0200

    updates in batch function

--- files/batch_functions/batch_dwesn.jl
+++ files/batch_functions/batch_dwesn.jl
@@ -10,6 +10,8 @@ function do_batch_dwesn(_params_esn, _params)
                     ,R      = new_R(p[:layers][1][i], density=pe[:density][1][i], rho=pe[:rho][1][i], gpu=p[:gpu])
                     ,R_in   = new_R_in(p[:layers][1][i], im_sz , sigma = pe[:sigma][1][i] ,gpu=p[:gpu], density=pe[:Rin_dens][1][i])
                     ,R_scaling = pe[:R_scaling][1][i], alpha = pe[:alpha][1][i], rho = pe[:rho][1][i], sigma = pe[:sigma][1][i], sgmd = pe[:sgmds][1][i]
+                    ,input_active = i in p[:active_inputs]
+                    ,output_active= i in p[:active_outputs]
                 ) for i in 1:length(p[:layers][1])
             ])
     push!(layers,layer1)
@@ -28,6 +30,8 @@ function do_batch_dwesn(_params_esn, _params)
                 ,R      = new_R(p[:layers][l][i], density=pe[:density][l][i], rho=pe[:rho][l][i], gpu=p[:gpu])
                 ,R_in   = new_R_in(p[:layers][l][i], fsz(id_counter + i) , sigma = pe[:sigma][l][i] ,gpu=p[:gpu], density=pe[:Rin_dens][l][i] )
                 ,R_scaling = pe[:R_scaling][l][i], alpha  = pe[:alpha][l][i], rho = pe[:rho][l][i], sigma = pe[:sigma][l][i], sgmd = pe[:sgmds][l][i]
+                ,input_active = (id_counter + i) in p[:active_inputs]
+                ,output_active= (id_counter + i) in p[:active_outputs]
             ) for i in 1:length(p[:layers][l])
         ])
         push!(layers,layer)
