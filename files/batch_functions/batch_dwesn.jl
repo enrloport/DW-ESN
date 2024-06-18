749698201d631f7cff12f9df18cbecebb8abb9cc
enrloport <e.l.o.universidad@gmail.com>
2024-06-18 12:08:20 +0200

    updates in batch_dwesn

--- files/batch_functions/batch_dwesn.jl
+++ files/batch_functions/batch_dwesn.jl
@@ -23,20 +23,6 @@ function do_batch_dwesn(_params_esn, _params)
         ])
         push!(layers,layer)
 
-        # for i in 1:length(p[:layers][l])
-        #     _esns = []
-        #     _e = ESN( 
-        #             id     = id_counter + i
-        #             ,R      = new_R(p[:layers][l][i], density=pe[:density][l][i], rho=pe[:rho][l][i], gpu=p[:gpu])
-        #             ,R_in   = new_R_in(p[:layers][l][i], fsz(id_counter + i) , sigma = pe[:sigma][l][i] ,gpu=p[:gpu], density=pe[:Rin_dens][l][i] )
-        #             ,R_scaling = pe[:R_scaling][l][i], alpha  = pe[:alpha][l][i], rho = pe[:rho][l][i], sigma = pe[:sigma][l][i], sgmd = pe[:sgmds][l][i]
-        #             ,input_active = (id_counter + i) in p[:active_inputs]
-        #             ,output_active= (id_counter + i) in p[:active_outputs]
-        #         )
-        #     push!(_e,_esns)
-        # end
-
-        # push!(layers,layerESN(esns = _esns))
     end   
 
     dwE = DWESN(
