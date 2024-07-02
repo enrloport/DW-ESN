08535b53cea41f3a30e3550c9c7eb460c646399a
enrloport <e.l.o.universidad@gmail.com>
2024-07-02 07:54:54 +0200

    updates in pso_newcastle

--- runs/newcastle/pso_newcastle_GPU.jl
+++ runs/newcastle/pso_newcastle_GPU.jl
@@ -43,9 +43,9 @@ _imgs, _hum, _windS, _press = _imgs[1:maxi,:,:], _hum[1:maxi]./10.0, _windS[1:ma
 repit = 1
 _params = Dict{Symbol,Any}(
      :gpu               => true
-    ,:wb                => false
+    ,:wb                => true
     ,:confusion_matrix  => false
-    ,:wb_logger_name    => "pso_DWESN_cloudcast_pixel_H1to4-100_GPU"
+    ,:wb_logger_name    => "pso_newcastle_GPU"
     ,:classes           => [0,1,2,3,4,5,6,7,8,9,10]
     ,:beta              => 1.0e-8
     ,:initial_transient => 1000
@@ -120,7 +120,7 @@ function fitness(_x)
     _params[:active_inputs] = [1,2,3,4,5,6,7]
     _params[:active_outputs]= [6,7]
 
-    sd = 42 #rand(1:10000)
+    sd = 31 #rand(1:10000)
     Random.seed!(sd)
 
     _params_esn = Dict{Symbol,Any}(
@@ -141,6 +141,12 @@ function fitness(_x)
         , "Test length"         => _params[:test_length]
         , "Radius"              => _params[:radius]
         , "Initial transient"   => _params[:initial_transient]
+        , "Active inputs"       => _params[:active_inputs]
+        , "Active outputs"      => _params[:active_outputs]
+        , "Total time"          => _params[:total_time]
+        , "Train time"          => _params[:train_time]
+        , "Test time"           => _params[:test_time]
+        , "Layers"              => _params[:layers]
         , "Sigmoids"            => _params_esn[:sgmds]
         , "Alphas"              => _params_esn[:alpha]
         , "Densities"           => _params_esn[:density]
@@ -148,23 +154,39 @@ function fitness(_x)
         , "Rhos"                => _params_esn[:rho]
         , "Sigmas"              => _params_esn[:sigma]
         , "R_scalings"          => _params_esn[:R_scaling]
-        , "Active inputs"       => _params[:active_inputs]
-        , "Active outputs"      => _params[:active_outputs]
-        )
+        , "Sigmoids"            => _params_esn[:sgmds]
+        , "Alphas"              => _params_esn[:alpha]
+        , "Densities"           => _params_esn[:density]
+        , "R_in_densities"      => _params_esn[:Rin_dens]
+        , "Rhos"                => _params_esn[:rho]
+        , "Sigmas"              => _params_esn[:sigma]
+        , "R_scalings"          => _params_esn[:R_scaling]
+        , "rho"                 => _params_esn[:rho][1][1]
+        , "sigma"               => _params_esn[:sigma][1][1]
+        , "reservoirs"          => sum([x[1] for x in _params[:layers]])
+        , "nodes"               => sum( [ sum(l) for l in _params[:layers] ] )
+        , "alpha min"           => minimum( vcat( _params_esn[:alpha]...) )
+        , "alpha max"           => maximum( vcat( _params_esn[:alpha]...) )
+        , "density min"         => minimum( vcat( _params_esn[:density]...) )
+        , "density max"         => maximum( vcat( _params_esn[:density]...) )
+    )
+    merge!(to_log,err_dict)
     edges = Dict( "Edge "*string(i) => _x[i] for i in 1:length(_x) )
-
-    if _params[:wb]
-        _params[:lg] = wandb_logger(_params[:wb_logger_name])
-        Wandb.log(_params[:lg], merge(par,edges) )
-    end
-    display(par)
 
     tm = @elapsed begin
         dwE = do_batch_dwesn(_params_esn,_params)
     end
 
+    err_dict = Dict("Error_step_"*string(s) => dwE.error[s] for s in params[:steps] )
+
+    if _params[:wb]
+    #    _params[:lg] = wandb_logger(_params[:wb_logger_name])
+        Wandb.log(_params[:lg], merge(par,edges, err_dict) )
+    end
+    display(par)
+
     _params[:total_time] = tm
-    full_log(_params,_params_esn,dwE)
+    # full_log(_params,_params_esn,dwE)
 
     printime = _params[:gpu] ? "Time GPU: " * string(tm) :  "Time CPU: " * string(tm) 
     println("Error: ", dwE.error, "\n", printime  )
