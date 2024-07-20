3f7e53b07b08b1060b68c60a3516299a3ba3b558
enrloport <e.l.o.universidad@gmail.com>
2024-07-20 19:11:14 +0200

    updates in dwesnia_cloudcast_pixel

--- runs/cloudcast/DWESNIA/DWESNIA_cloudcast_pixel_GPU.jl
+++ runs/cloudcast/DWESNIA/DWESNIA_cloudcast_pixel_GPU.jl
@@ -45,7 +45,7 @@ for _l in [2,3,4,5]
 
     for _ in 1:repit
         dwE=[]
-        _params[:layers] = [ [_w,300] for _ in 1:_l ]
+        _params[:layers] = [ [300 for _ in 1:_w] for _ in 1:_l ]
         _params[:connections] = Dict(
             (x+1) => [((x-_w+1),1.0)]
             for x in _w:(_l*_w)-1
