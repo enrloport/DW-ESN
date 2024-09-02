be120f03a5ad04465d0e2e7efb1dfcc64623ca1e
enrloport <e.l.o.universidad@gmail.com>
2024-09-02 08:20:25 +0200

    updates in test_dwesn function

--- files/test_functions/test_DWESN_cloudcast_image.jl
+++ files/test_functions/test_DWESN_cloudcast_image.jl
@@ -1,15 +1,24 @@
 # Function to test an already trained deepwideESN struct
 function __do_test_DWESN_cloudcast_image!(dwE, args::Dict)
-    test_length = args[:test_length]
-    classes_Y   = Dict( stp => Array{Tuple{Float64,Int,Int}}[] for stp in args[:steps])
-    wrong_class = Dict( stp => [] for stp in args[:steps])
-    dwE.Y       = Dict( stp => [] for stp in args[:steps])
     f           = args[:gpu] ? (u) -> CuArray(reshape(u, :, 1)) : (u) -> reshape(u, :, 1)
+    test_length = args[:test_length]
+    sz1,sz2     = size(args[:data])[2], size(args[:data])[3]
 
-    d1,d2,d3 = test_length, size(args[:data])[2], size(args[:data])[3]
-    res = Dict(stp => zeros(d1,d2,d3) for stp in args[:steps])
+    classes_Y   = Dict( 
+        (i,j) => Dict( stp => Array{Tuple{Float64,Int,Int}}[] for stp in args[:steps])
+        for i in 1:sz1 for j in 1:sz2 
+        )
+    wrong_class = Dict( 
+        (i,j) => Dict( stp => [] for stp in args[:steps] )
+        for i in 1:sz1 for j in 1:sz2 
+        )
 
-    for i in 1+args[:radius]:d2-args[:radius], j in 1+args[:radius]:d3-args[:radius]
+    dwE.Y       = Dict( 
+        (i,j) => Dict( stp => [] for stp in args[:steps] )
+        for i in 1:sz1 for j in 1:sz2 
+        )
+
+    for i in 1+args[:radius]:sz1-args[:radius], j in 1+args[:radius]:sz2-args[:radius]
         args[:target_pixel] = (i,j)
         args[:train_data],  args[:train_labels],  args[:test_data],  args[:test_labels] = split_data_cloudcast(
         data              = args[:data]
@@ -42,23 +51,18 @@ function __do_test_DWESN_cloudcast_image!(dwE, args::Dict)
                 pairs_sorted  = reverse(sort(pairs[stp]))
 
                 if pairs_sorted[1][2] != pairs_sorted[1][3]
-                    push!(wrong_class[stp], (args[:test_data][t], pairs_sorted[1], pairs_sorted[2], t ) ) 
+                    push!(wrong_class[(i,j)][stp], (args[:test_data][t], pairs_sorted[1], pairs_sorted[2], t ) ) 
                 end
 
-                push!(dwE.Y[stp],[Int8(pairs_sorted[1][2]) ;])
-                push!(classes_Y[stp], pairs[stp] )
+                push!(dwE.Y[(i,j)][stp],[Int8(pairs_sorted[1][2]) ;])
+                push!(classes_Y[(i,j)][stp], pairs[stp] )
 
-                res[stp][t,i,j] = Int8(pairs_sorted[1][2])
             end
-
-
-
         end
     end
-
-    dwE.Y = res
 
     dwE.wrong_class= wrong_class
     dwE.classes_Y  = classes_Y
     dwE.Y_target   = args[:test_labels]
+    dwE.error      = Dict( stp => [length(wrong_class[(i,j)][stp]) / length(classes_Y[(i,j)][stp]) for i in 1:sz1 for j in 1:sz2] for stp in args[:steps]   )
 end
