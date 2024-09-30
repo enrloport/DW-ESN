b0279bac15367aac945434c1623b34cda15edb9b
enrloport <e.l.o.universidad@gmail.com>
2024-09-30 11:15:15 +0200

    updates in split_cloudcast

--- files/cloudcast_functions/split_dataset.jl
+++ files/cloudcast_functions/split_dataset.jl
@@ -1,13 +1,26 @@
 
-function split_data_cloudcast(;data, train_length, test_length, target_pixel, radius, steps=[1])
+function split_data_cloudcast(;data, train_length, test_length, target_pixel, radius, steps=[1], train_start=1)
 
-    d,tp,rd,trl,tel = data,target_pixel, radius, train_length, test_length
+    d,tp,rd,trl,tel,trs = data,target_pixel, radius, train_length, test_length, train_start
 
-    train_x   = cc_to_int(d[1:trl         , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
-    test_x    = cc_to_int(d[trl+1:trl+tel , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
+    train_x   = cc_to_int(d[trs:trs+trl         , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
+    test_x    = cc_to_int(d[trs+trl+1:trs+trl+tel , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
 
-    train_y = Dict(s => cc_to_int(d[1+s:trl+s        , tp[1], tp[2]]) for s in steps)
-    test_y  = Dict(s => cc_to_int(d[trl+1+s:trl+tel+s, tp[1], tp[2]]) for s in steps)
+    train_y = Dict(s => cc_to_int(d[trs+s:trs+trl+s        , tp[1], tp[2]]) for s in steps)
+    test_y  = Dict(s => cc_to_int(d[trs+trl+1+s:trs+trl+tel+s, tp[1], tp[2]]) for s in steps)
     
     return train_x, train_y, test_x, test_y
 end
+
+# function split_data_cloudcast(;data, train_length, test_length, target_pixel, radius, steps=[1])
+
+#     d,tp,rd,trl,tel = data,target_pixel, radius, train_length, test_length
+
+#     train_x   = cc_to_int(d[1:trl         , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
+#     test_x    = cc_to_int(d[trl+1:trl+tel , tp[1]-rd:tp[1]+rd , tp[2]-rd:tp[2]+rd ])
+
+#     train_y = Dict(s => cc_to_int(d[1+s:trl+s        , tp[1], tp[2]]) for s in steps)
+#     test_y  = Dict(s => cc_to_int(d[trl+1+s:trl+tel+s, tp[1], tp[2]]) for s in steps)
+    
+#     return train_x, train_y, test_x, test_y
+# end
