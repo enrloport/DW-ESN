f931ceaf46b9de1059ce25000e517c4804ba5dae
enrloport <e.l.o.universidad@gmail.com>
2024-10-02 11:51:34 +0200

    updates in test_image function

--- files/test_functions/test_DWESN_cloudcast_image.jl
+++ files/test_functions/test_DWESN_cloudcast_image.jl
@@ -9,7 +9,8 @@ function __do_test_DWESN_cloudcast_image!(dwE, args::Dict)
         for i in 1:sz1 for j in 1:sz2 
         )
     wrong_class = Dict( 
-        (i,j) => Dict( stp => [] for stp in args[:steps] )
+        # (i,j) => Dict( stp => [] for stp in args[:steps] )
+        (i,j) => Dict( stp => 0 for stp in args[:steps] )
         for i in 1:sz1 for j in 1:sz2 
         )
 
@@ -19,6 +20,7 @@ function __do_test_DWESN_cloudcast_image!(dwE, args::Dict)
         )
 
     for i in 1+args[:radius]:sz1-args[:radius], j in 1+args[:radius]:sz2-args[:radius]
+        println( "Pixel (" *string(i)* "," *string(j)* ")" )
         args[:target_pixel] = (i,j)
         args[:train_data],  args[:train_labels],  args[:test_data],  args[:test_labels] = split_data_cloudcast(
         data              = args[:data]
@@ -53,11 +55,12 @@ function __do_test_DWESN_cloudcast_image!(dwE, args::Dict)
                 pairs_sorted  = reverse(sort(pairs[stp]))
 
                 if pairs_sorted[1][2] != pairs_sorted[1][3]
-                    push!(wrong_class[(i,j)][stp], (args[:test_data][t], pairs_sorted[1], pairs_sorted[2], t ) ) 
+                    # push!(wrong_class[(i,j)][stp], (args[:test_data][t], pairs_sorted[1], pairs_sorted[2], t ) )
+                    wrong_class[(i,j)][stp] += 1
                 end
 
-                push!(dwE.Y[(i,j)][stp],[Int8(pairs_sorted[1][2]) ;])
-                push!(classes_Y[(i,j)][stp], pairs[stp] )
+                # push!(dwE.Y[(i,j)][stp],[Int8(pairs_sorted[1][2]) ;])
+                # push!(classes_Y[(i,j)][stp], pairs[stp] )
 
             end
         end
@@ -66,5 +69,6 @@ function __do_test_DWESN_cloudcast_image!(dwE, args::Dict)
     dwE.wrong_class= wrong_class
     dwE.classes_Y  = classes_Y
     dwE.Y_target   = args[:test_labels]
-    dwE.error      = Dict( stp => [length(wrong_class[(i,j)][stp]) / length(classes_Y[(i,j)][stp]) for i in 1:sz1 for j in 1:sz2] for stp in args[:steps]   )
+    # dwE.error      = Dict( stp => [length(wrong_class[(i,j)][stp]) / length(classes_Y[(i,j)][stp]) for i in 1:sz1 for j in 1:sz2] for stp in args[:steps]   )
+    dwE.error      = Dict( stp => [wrong_class[(i,j)][stp] / test_length for i in 1:sz1 for j in 1:sz2] for stp in args[:steps]   )
 end
